719fd141f776a12afd18a7085ed51c1c097bf85c
RiditCodes <90456597+RiditCodes@users.noreply.github.com>
2024-04-26 19:14:23 +0530

    New and old

---
+++ 4_LED_Control/4_LED_Control.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+}
