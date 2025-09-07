628f782455860757e450f90251e33a8b5b1efdb8
RiditCodes <90456597+RiditCodes@users.noreply.github.com>
2025-09-07 12:53:31 +0530

    new files

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -1,14 +1,16 @@
 #include <Servo.h>
 
-int servoPin = 3; 
+int servoPin = 3;
+int potPin = 0;
+int val;
  
 Servo Servo1; 
 void setup() {  
    Servo1.attach(servoPin); 
 }
-void loop(){ 
-   Servo1.write(0); 
-   delay(1000); 
-   Servo1.write(180); 
-   delay(1000); 
+void loop(){
+   val = analogRead(potPin);
+   val = map(val, 0, 1023, 0, 180);
+   Servo1.write(val);
+   delay(15);
 }
